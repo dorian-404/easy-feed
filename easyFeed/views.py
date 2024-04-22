264febcf375e8e4b7a5331cdbb36c28ec581b09d
Yannis <yantd29@gmail.com>
2024-04-22 12:59:18 -0300

    Ajout des fichiers home.html et home.css dans les repertoires html et css respectivement

--- easyFeed/views.py
+++ easyFeed/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'html/index.html', context={})
+    return render(request, 'html/home.html', context={})
