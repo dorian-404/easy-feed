0c4fe05e89c356d78ba6be23ab1c8e4f08be85ff
dorian <mishaelwontcheu@gmail.com>
2024-05-06 13:25:09 -0300

    changement du nom des vues

--- easyFeed/views.py
+++ easyFeed/views.py
@@ -3,13 +3,13 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def home_view(request):
+def home(request):
     return render(request, 'html/home.html', context={})
 
 
-def login_view(request):
+def login(request):
     return render(request, 'html/login.html', context={})
 
 
-def dashboard_view(request):
+def dashboard(request):
     return render(request, 'html/dashboard.html', context={})
